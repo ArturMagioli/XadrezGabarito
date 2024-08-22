6e3e3a69faaf03fba5799cb7031012af078ee39f
Artur de Paula <arturdepaula00@gmail.com>
2024-08-22 18:15:50 -0300

    Movimentando peças.

--- src/chess/ChessException.java
+++ src/chess/ChessException.java
@@ -1,6 +1,15 @@
 package chess;
 
-public class ChessException extends RuntimeException {
+import boardgame.BoardException;
+
+public class ChessException extends BoardException {
+	/*
+	 * ChessException como subclasse de BoardException garante que os tratamentos
+	 * de exceções aninhados são aplicados tanto nas exçeções ocorridas em Board 
+	 * quanto nas de Chess. Segundo o professor, lógicamente isso é correto, pois
+	 * os problemas que ocorrem na camada Chess são lógicamente, também da camada 
+	 * camda Board.
+	 */
 	private static final long serialVersionUID = 1L;
 	
 	public ChessException(String msg) {
